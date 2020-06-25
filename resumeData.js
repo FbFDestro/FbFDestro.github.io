a3b625c83e4abe369ffed4a4759c208ef740fe82
FbFDestro <fbfdestro@gmail.com>
2020-06-25 12:44:57 -0300

    update data

--- resumeData.js
+++ resumeData.js
@@ -8,9 +8,9 @@ const resume = [
         descriptions: [
           {
             info: 'Bachelor of Science in Computer Science',
-            rightInfo: 'GPA 8.8/10 - 4th in class (out of 127)'
-          }
-        ]
+            rightInfo: 'GPA 8.8/10 - 4th in class (out of 127)',
+          },
+        ],
       },
       {
         title: 'Linköping University (Sweden)',
@@ -18,29 +18,29 @@ const resume = [
         descriptions: [
           {
             info:
-              'One semester studying abroad taking Computer Science courses as an exchange student.'
-          }
-        ]
+              'One semester studying abroad taking Computer Science courses as an exchange student.',
+          },
+        ],
       },
       {
         title: 'Escola Tecnica Everardo Passos (ETEP)',
         rightTitle: '2014 - 2016',
         descriptions: [
           {
-            info: 'Computer Technician'
-          }
-        ]
-      }
-    ]
+            info: 'Computer Technician',
+          },
+        ],
+      },
+    ],
   },
   {
     title: 'Work Experience',
     data: [
       {
         title: 'Incoming Software Engineer Intern at Facebook (London - UK)',
-        rightTitle: 'May/2020 - July/2020'
-      }
-    ]
+        rightTitle: 'July/2021 - Oct./2021',
+      },
+    ],
   },
   {
     title: 'Major Projects',
@@ -52,9 +52,20 @@ const resume = [
           {
             info: `Chrome extension that automatically likes or dislikes youtube videos from 
                       selected channels when you watch them. Developed mainly with Javascript 
-                      and Chrome Browser API. Published at Chrome Web Store with real users.`
-          }
-        ]
+                      and Chrome Browser API. Published at Chrome Web Store with real users.`,
+          },
+        ],
+      },
+      {
+        title: `<a href="https://github.com/FbFDestro/news-program-manager" target='_blank'>News program manager</a>`,
+        rightTitle: '2019 - 2020',
+        descriptions: [
+          {
+            info: `Developed for learning purposes to the Database course focused
+             on complex queries and modeling. The project has a React.js frontend 
+             that makes requests to a Node.js RESTful backend which query a PostgreSQL database. `,
+          },
+        ],
       },
       {
         title: `<a href="https://github.com/FbFDestro/DownloadBanker" target='_blank'>DownloadBanker</a>`,
@@ -64,18 +75,18 @@ const resume = [
             info: `E-commerce of authorial digital content with the goal of being an 
                       intermediary between producers and consumers.
                       The project has a Website (Asp.Net, C#, and Javascript),
-                      an Android App (Java) and a Desktop Application (C#).`
-          }
-        ]
+                      an Android App (Java) and a Desktop Application (C#).`,
+          },
+        ],
       },
       {
         title: `<a href="https://github.com/FbFDestro/TechFit" target='_blank'>TechFit</a>`,
         rightTitle: '2015',
         descriptions: [
           {
-            info: 'Desktop gym manager developed with C# and MySQL'
-          }
-        ]
+            info: 'Desktop gym manager developed with C# and MySQL',
+          },
+        ],
       },
       {
         title: `<a href="https://github.com/FbFDestro/FirstPlei" target='_blank'>FirstPlei</a>`,
@@ -83,11 +94,11 @@ const resume = [
         descriptions: [
           {
             info:
-              'Desktop games including Sudoku, Memory Game and Tic Tac Toe developed with C#'
-          }
-        ]
-      }
-    ]
+              'Desktop games including Sudoku, Memory Game and Tic Tac Toe developed with C#',
+          },
+        ],
+      },
+    ],
   },
   {
     title: 'Activities',
@@ -99,30 +110,29 @@ const resume = [
           {
             info: `Helped professor with development and correction of tests and exercises for Introduction 
                   to Computer Science I ​ and Introduction to Computer Science: Laboratory Practice I. 
-                  Gave classes for around 60 students to reinforce understanding of subjects and solve doubts.`
-          }
-        ]
+                  Gave classes for around 60 students to reinforce understanding of subjects and solve doubts.`,
+          },
+        ],
       },
       {
         title: 'Brazilian ICPC Summer School',
         rightTitle: 'Unicamp – Jan./2018',
         descriptions: [
           {
-            info: 'Two weeks training for competitive programming contests'
-          }
-        ]
+            info: 'Two weeks training for competitive programming contests',
+          },
+        ],
       },
       {
-        title:
-          'Member at University Study Group for Programming Contests (GEMA)',
+        title: 'Member at University Study Group for Programming Contests (GEMA)',
         rightTitle: 'Mar/2017 - Dec/2018',
         descriptions: [
           {
-            info: 'Weekly meetings and contests to exercise problem-solving.'
-          }
-        ]
-      }
-    ]
+            info: 'Weekly meetings and contests to exercise problem-solving.',
+          },
+        ],
+      },
+    ],
   },
   {
     title: 'Achievements',
@@ -133,29 +143,28 @@ const resume = [
           {
             info:
               '7th place in local scoreboard/53rd place in global scoreboard out of 1556',
-            rightInfo: '2018'
+            rightInfo: '2018',
           },
           {
             info:
               '9th place in local scoreboard/93rd place in global scoreboard out of 873',
-            rightInfo: '2017'
-          }
-        ]
+            rightInfo: '2017',
+          },
+        ],
       },
       {
         title: 'Brazilian Informatics Olympiad - OBI',
         descriptions: [
           {
             info: '76th place out of 1251 participants - University level',
-            rightInfo: '2017'
+            rightInfo: '2017',
           },
           {
-            info:
-              '40th place out of 122 participants (honorable mention) - Junior level',
-            rightInfo: '2012'
-          }
-        ]
-      }
-    ]
-  }
+            info: '40th place out of 122 participants (honorable mention) - Junior level',
+            rightInfo: '2012',
+          },
+        ],
+      },
+    ],
+  },
 ];
